ef5c387e3dc11dd034b859fff519ad615b9deafa
DESKTOP-52351T8\Admin <kts6376@naver.com>
2024-10-11 17:30:46 +0900

    1. 플레이어 대기, 걷기, 뛰기 애니메이션 2. 플레이어 반전 구현 3. 플레이어 점프 애니 셋팅

--- Assets/My2D/Scripts/PlayerController.cs
+++ Assets/My2D/Scripts/PlayerController.cs
@@ -7,18 +7,69 @@ namespace My2D
     {
         #region Variables
         private Rigidbody2D rb2D;
+        private Animator animator;
 
         //플레이어 걷기 속도
         [SerializeField] private float walkSpeed = 4f;
 
         //플레이어 이동과 관련된 입력값
         private Vector2 inputMove;
+
+        //걷기
+        [SerializeField] private bool isMove = false;
+        public bool IsMove
+        {
+            get
+            {
+                return isMove;
+            }
+            set
+            {
+                isMove = value;
+                animator.SetBool(AnimationString.IsMove, value);
+            }
+        }
+
+        //뛰기
+        [SerializeField] private bool isRun = false;
+        public bool IsRun
+        {
+            get
+            {
+                return isRun;
+            }
+            set
+            {
+                isRun = value;
+                animator.SetBool(AnimationString.IsRun, value);
+            }
+        }
+
+        //좌우 반전
+        [SerializeField] private bool isFacingRight = true;
+        public bool IsFacingRight
+        {
+            get
+            {
+                return isFacingRight;
+            }
+            set
+            {
+                //반전
+                if (isFacingRight != value)
+                {
+                    transform.localScale *= new Vector2(-1, 1);
+                }
+                isFacingRight = value;
+            }
+        }
         #endregion
 
         private void Awake()
         {
             //참조
-            rb2D = GetComponent<Rigidbody2D>();
+            rb2D = this.GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
             //rb2D.velocity
         }
 
@@ -28,10 +79,41 @@ namespace My2D
             rb2D.velocity = new Vector2(inputMove.x * walkSpeed, rb2D.velocity.y);
         }
 
+        //바라보는 방향을 전환
+        void SetFacingDirection(Vector2 moveInput)
+        {
+            if (moveInput.x > 0f && isFacingRight == false)
+            {
+                //오른쪽을 바라본다
+                isFacingRight = true;
+            }
+            else if (moveInput.x < 0f && isFacingRight == true)
+            {
+                //왼쪽을 바라본다
+                isFacingRight = false;
+            }
+        }
+
         public void OnMove(InputAction.CallbackContext context)
         {
             inputMove = context.ReadValue<Vector2>();
-            Debug.Log("inputMovecontext : " + inputMove);
+            IsMove = (inputMove != Vector2.zero);
+
+            //방향전환
+            SetFacingDirection(inputMove);
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            //누르기 시작하는 순간
+            if(context.started)
+            {
+               IsRun = true;
+            }
+            else if(context.canceled)   //릴리즈 하는 순간
+            {
+                IsRun= false;
+            }
         }
     }
 }
